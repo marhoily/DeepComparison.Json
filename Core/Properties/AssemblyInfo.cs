7f31e79758e190e081ec64b0d5c35a6a8261021a
marhoily <marhoily@gmail.com>
2016-11-29 14:05:37 +0300

    build test works

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyTitle("DeepComparisong.Json")]
 [assembly: AssemblyDescription(
     "Compares newtonsoft JToken objects with CLR objects (including anonymous)")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
